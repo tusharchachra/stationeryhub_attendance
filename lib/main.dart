d1d40157bf1275476be01b539510579740a04b9e
Tushar Chachra <tusharchachra@gmail.com>
2024-04-10 12:49:53 +0530

    send otp

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +11,9 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseAppCheck.instance.activate(
+      webProvider:
+          ReCaptchaV3Provider('6LdiirYpAAAAAFZ1pyLKhZEcZpp2w6x_PullHH5r'));
   runApp(const StationeryHubAttendance());
 }
 
