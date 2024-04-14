673c7954f6d79688943ddc5104a2c90523f143e7
Tushar Chachra <tusharchachra@gmail.com>
2024-04-15 02:41:12 +0530

    fetch geo location

--- lib/screens/screen_new_organization.dart
+++ lib/screens/screen_new_organization.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:stationeryhub_attendance/form_fields/form_field_button.dart';
 import 'package:stationeryhub_attendance/scaffold/scaffold_home.dart';
 
 class NewOrganizationScreen extends StatefulWidget {
@@ -13,6 +15,7 @@ class _NewOrganizationScreenState extends State<NewOrganizationScreen> {
   TextEditingController nameController = TextEditingController();
   TextEditingController addressController = TextEditingController();
   TextEditingController geoLocationController = TextEditingController();
+  Position? _currentPosition;
   @override
   Widget build(BuildContext context) {
     return ScaffoldHome(
@@ -38,9 +41,59 @@ class _NewOrganizationScreenState extends State<NewOrganizationScreen> {
             decoration: InputDecoration(
               label: Text('Geo location'),
             ),
-          )
+          ),
+          FormFieldButton(
+              width: 30,
+              height: 10,
+              buttonText: 'Fetch location',
+              onTapAction: () async {
+                await _getCurrentPosition();
+                print(_currentPosition);
+              },
+              buttonDecoration: BoxDecoration(),
+              textStyle: TextStyle())
         ],
       ),
     );
+  }
+
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> _getCurrentPosition() async {
+    final hasPermission = await _handleLocationPermission();
+    if (!hasPermission) return;
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) {
+      setState(() => _currentPosition = position);
+    }).catchError((e) {
+      debugPrint(e);
+    });
   }
 }
