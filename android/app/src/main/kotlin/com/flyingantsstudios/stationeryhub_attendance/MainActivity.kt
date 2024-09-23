587517ad4fde4d029afa2bd0101b28ed8043218a
Tushar Chachra <tusharchachra@gmail.com>
2024-09-23 14:34:37 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/flyingantsstudios/stationeryhub_attendance/MainActivity.kt
+++ android/app/src/main/kotlin/com/flyingantsstudios/stationeryhub_attendance/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.flyingantsstudios.stationeryhub_attendance
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
