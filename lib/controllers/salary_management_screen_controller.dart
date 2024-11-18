7107316195df5321e28d1cbc0ef806163b2befad
Tushar Chachra <tusharchachra@gmail.com>
2024-11-18 19:46:22 +0530

    salary management screen
    completed

--- lib/controllers/salary_management_screen_controller.dart
+++ lib/controllers/salary_management_screen_controller.dart
@@ -1,3 +1,50 @@
 import 'package:get/get.dart';
+import 'package:stationeryhub_attendance/controllers/admin_dashboard_screen_controller.dart';
+import 'package:stationeryhub_attendance/models/users_model.dart';
 
-class SalaryManagementScreenController extends GetxController {}
+class SalaryManagementScreenController extends GetxController {
+  final AdminDashboardScreenController adminDashboardScreenController =
+      Get.find();
+  RxList<UsersModel> tempEmpList = <UsersModel>[].obs;
+  RxBool showTotal = false.obs;
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    loadEmpList();
+  }
+
+  void loadEmpList() {
+    tempEmpList.value = adminDashboardScreenController.employeeList;
+    tempEmpList.refresh();
+  }
+
+  @override
+  void onReady() {
+    // TODO: implement onReady
+    super.onReady();
+  }
+
+  void invertShowTotal() {
+    if (showTotal.value == false) {
+      showTotal.value = true;
+      //change to false after 5 seconds
+      Future.delayed(Duration(seconds: 5))
+          .then((val) => showTotal.value = false);
+    } else {
+      showTotal.value = false;
+    }
+  }
+
+  void filterList(String val) {
+    if (val == '') {
+      loadEmpList();
+    }
+    tempEmpList.value = adminDashboardScreenController.employeeList
+        .where((element) =>
+            element.name!.toLowerCase().contains(val.toLowerCase()))
+        .toList();
+    tempEmpList.refresh();
+  }
+}
--- lib/screens/salary_management_screen.dart
+++ lib/screens/salary_management_screen.dart
@@ -1,10 +1,19 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:stationeryhub_attendance/components/form_field_text.dart';
 import 'package:stationeryhub_attendance/components/picture_circle.dart';
 import 'package:stationeryhub_attendance/controllers/admin_dashboard_screen_controller.dart';
+import 'package:stationeryhub_attendance/controllers/salary_management_screen_controller.dart';
 import 'package:stationeryhub_attendance/helpers/constants.dart';
 import 'package:stationeryhub_attendance/scaffold/scaffold_dashboard.dart';
+import 'package:stationeryhub_attendance/screens/date_filter_dialog.dart';
+
+import '../components/filter_button.dart';
+import '../controllers/date_filter_dialog_controller.dart';
 
 class SalaryManagementScreen extends StatelessWidget {
   const SalaryManagementScreen({super.key});
@@ -13,7 +22,14 @@ class SalaryManagementScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final AdminDashboardScreenController adminDashboardScreenController =
         Get.find();
-    print(adminDashboardScreenController.employeeList.toString());
+    final salaryManagementScreenController =
+        Get.put(SalaryManagementScreenController());
+    final dateFilterDialogController = Get.put(DateFilterDialogController());
+
+    /* element.name
+                  .splitMapJoin(RegExp(r"(?<=[a-z])(?=[A-Z])"),
+                      onMatch: (m) => ' ')
+                  .capitalize!*/
     return ScaffoldDashboard(
       pageTitle: Text(
         'Manage Salary',
@@ -35,118 +51,251 @@ class SalaryManagementScreen extends StatelessWidget {
                   alignment: Alignment.centerRight,
                 ),
               ),
-              child: Column(
-                children: [
-                  Text('Total payout'),
-                  Text('\u{20B9}85000'),
-                ],
+              child: Obx(
+                () {
+                  String selectedDate = DateFormat('d MMM y, E')
+                      .format(dateFilterDialogController.selectedDate.value)
+                      .toString();
+                  String selectedRecent = dateFilterDialogController
+                      .filterRecent.value.name
+                      .splitMapJoin(RegExp(r"(?<=[a-z])(?=[A-Z])"),
+                          onMatch: (m) => ' ')
+                      .capitalize!;
+                  return Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 15.w, vertical: 10.h),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Total payout',
+                              style: Get.textTheme.titleMedium!
+                                  .copyWith(color: Colors.white),
+                            ),
+                            SizedBox(width: 10.w),
+                            dateFilterDialogController.filter.value ==
+                                    FilterOptions.calendar
+                                ? Text(selectedDate)
+                                : Text(selectedRecent),
+                            ImageFiltered(
+                              imageFilter: ImageFilter.blur(
+                                sigmaX: 5.0,
+                                sigmaY: 5.0,
+                              ),
+                              enabled: !salaryManagementScreenController
+                                  .showTotal.value,
+                              child: Text(
+                                '\u{20B9}85000',
+                                style: Get.textTheme.displayLarge!.copyWith(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Obx(
+                          () => GestureDetector(
+                            onTap: () {
+                              salaryManagementScreenController
+                                  .invertShowTotal();
+                            },
+                            behavior: HitTestBehavior.translucent,
+                            child: Icon(
+                              salaryManagementScreenController
+                                          .showTotal.value ==
+                                      true
+                                  ? Icons.visibility_off_outlined
+                                  : Icons.visibility_outlined,
+                              size: 30.w,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
               ),
             ),
           ),
-          Expanded(
-            child: ListView.builder(
-                itemCount: adminDashboardScreenController.employeeList.length,
-                itemBuilder: (context, index) {
-                  int salaryPerDay = (adminDashboardScreenController
-                              .employeeList[index].salary! /
-                          adminDashboardScreenController
-                              .getCurrentMonthDates(
-                                  month: DateTime.now().month,
-                                  year: DateTime.now().year)
-                              .length)
-                      .ceil();
-                  return Padding(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 6.h, horizontal: 11.w),
-                    child: Container(
-                      width: 1.sw,
-                      height: 73.h,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(12.w, 16.h, 24.w, 16.h),
+                  child: SizedBox(
+                    /* width: 278.w,
+                    height: 50.h,*/
+                    child: FormFieldText(
+                      hintText: 'Search employee',
+                      enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10.r),
-                        border: Border.all(color: Constants.colourBorderLight),
+                        borderSide:
+                            BorderSide(color: Constants.colourBorderMedium),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Wrap(
-                            crossAxisAlignment: WrapCrossAlignment.center,
-                            children: [
-                              Padding(
-                                padding:
-                                    EdgeInsets.fromLTRB(7.w, 5.h, 17.w, 5.h),
-                                child: PictureCircle(
-                                    height: 60.h,
-                                    width: 60.w,
-                                    imgPath: adminDashboardScreenController
-                                        .employeeList[index].profilePicPath!),
-                              ),
-                              Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    adminDashboardScreenController
-                                        .employeeList[index].name!,
-                                    style: Get.textTheme.headlineLarge!
-                                        .copyWith(
-                                            color: Constants.colourTextDark),
-                                  ),
-                                  Row(
-                                    children: [
-                                      Image.asset(
-                                          'assets/images/salary_management_screen/bag.png'),
-                                      SizedBox(width: 8.w),
-                                      Text(
-                                        adminDashboardScreenController
-                                            .employeeList[index]
-                                            .userType!
-                                            .name
-                                            .capitalizeFirst!,
-                                        style: Get.textTheme.titleMedium!
-                                            .copyWith(
-                                                color:
-                                                    Constants.colourTextMedium),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 17.0.w),
-                            child: Align(
-                              alignment: Alignment.centerRight,
-                              child: Wrap(
-                                children: [
-                                  Text(
-                                    salaryPerDay.toString(),
-                                    style:
-                                        Get.textTheme.headlineLarge!.copyWith(
-                                      color: Constants.colourDashboardBox2,
-                                    ),
-                                  ),
-                                  Text(
-                                    '/day',
-                                    style:
-                                        Get.textTheme.headlineLarge!.copyWith(
-                                      color: Constants.colourTextDark,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                          )
-                        ],
+                      trailingWidget: Icon(
+                        Icons.search,
+                        color: Constants.colourBorderMedium,
                       ),
+                      onChangedAction: (val) {
+                        salaryManagementScreenController.filterList(val!);
+                      },
                     ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Get.dialog(
+                    DateFilterDialog(),
                   );
-                }),
+                },
+                child: Padding(
+                  padding: EdgeInsets.only(right: 12.w),
+                  child: Container(
+                    width: 104.w,
+                    height: 50.h,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      border: Border.all(
+                        color: Constants.colourBorderMedium,
+                      ),
+                      borderRadius: BorderRadius.circular(10.r),
+                    ),
+                    child: FilterButton(),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Obx(
+            () => Expanded(
+              child: salaryManagementScreenController.tempEmpList.isEmpty
+                  ? Center(
+                      child: Text(
+                        'No employee found',
+                        style: Get.textTheme.displaySmall!
+                            .copyWith(color: Constants.colourTextLight),
+                      ),
+                    )
+                  : buildEmpList(salaryManagementScreenController,
+                      adminDashboardScreenController),
+            ),
           )
         ],
       ),
     );
+  }
+
+  ListView buildEmpList(
+      SalaryManagementScreenController salaryManagementScreenController,
+      AdminDashboardScreenController adminDashboardScreenController) {
+    return ListView.builder(
+        itemCount: salaryManagementScreenController.tempEmpList.length,
+        itemBuilder: (context, index) {
+          int salaryPerDay =
+              (salaryManagementScreenController.tempEmpList[index].salary! /
+                      adminDashboardScreenController
+                          .getCurrentMonthDates(
+                              month: DateTime.now().month,
+                              year: DateTime.now().year)
+                          .length)
+                  .ceil();
+          return Padding(
+            padding: EdgeInsets.symmetric(vertical: 6.h, horizontal: 11.w),
+            child: Container(
+              width: 1.sw,
+              height: 73.h,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.r),
+                border: Border.all(color: Constants.colourBorderLight),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Wrap(
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(7.w, 5.h, 17.w, 5.h),
+                        child: PictureCircle(
+                            height: 60.h,
+                            width: 60.w,
+                            imgPath: salaryManagementScreenController
+                                .tempEmpList[index].profilePicPath!),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            salaryManagementScreenController
+                                .tempEmpList[index].name!,
+                            style: Get.textTheme.headlineLarge!
+                                .copyWith(color: Constants.colourTextDark),
+                          ),
+                          Row(
+                            children: [
+                              Image.asset(
+                                  'assets/images/salary_management_screen/bag.png'),
+                              SizedBox(width: 8.w),
+                              Text(
+                                salaryManagementScreenController
+                                    .tempEmpList[index]
+                                    .userType!
+                                    .name
+                                    .capitalizeFirst!,
+                                style: Get.textTheme.titleMedium!.copyWith(
+                                    color: Constants.colourTextMedium),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 17.0.w),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Wrap(
+                        children: [
+                          Obx(
+                            () => ImageFiltered(
+                              imageFilter: ImageFilter.blur(
+                                sigmaX: 5.0,
+                                sigmaY: 5.0,
+                              ),
+                              enabled: !salaryManagementScreenController
+                                  .showTotal.value,
+                              child: Text(
+                                salaryPerDay.toString(),
+                                style: Get.textTheme.headlineLarge!.copyWith(
+                                  color: Constants.colourDashboardBox2,
+                                ),
+                              ),
+                            ),
+                          ),
+                          /* Text(
+                            '/day',
+                            style: Get.textTheme.headlineLarge!.copyWith(
+                              color: Constants.colourTextDark,
+                            ),
+                          )*/
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
